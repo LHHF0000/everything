2152e2525ea763c014a24e53ca06978c2cde519a
LHHF0000 <2293840381@qq.com>
2023-06-06 20:40:20 +0800

    update excelA by excelB: style

--- python1/src/updExcel.py
+++ python1/src/updExcel.py
@@ -4,28 +4,29 @@ from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
 
 
-def deal(s):
-    i = 2
-    while i < rows + 1:
+def deal(c):
+    r = 2
+    while r < rows + 1:
         for row_b in worksheet_b.iter_rows(min_row=2, values_only=True):
             table_name_b = row_b[0]
             column_name_b = row_b[1]
             data_type_b = row_b[2]
 
-            col1 = sheet1.cell(row=i, column=s-2).value  # 获取(row,col)单元格的值
-            col2 = sheet1.cell(row=i, column=s-1).value
-            col3 = sheet1.cell(row=i, column=s).value
-            if col1 != table_name_b or col2 != column_name_b:
+            table_name_a = sheet1.cell(row=r, column=c - 2).value  # 获取(row,col)单元格的值
+            column_name_a = sheet1.cell(row=r, column=c - 1).value
+            data_type_a = sheet1.cell(row=r, column=c).value
+
+            # cell(r,c)中的行r和列c 是从1开始 而不是0 例如A11则是ws.cell(1,1),不是ws.cell(0,0)
+            if table_name_a != table_name_b or column_name_a != column_name_b:
                 continue
-            if col1 == table_name_b and col2 == column_name_b and col3 != data_type_b:
-                sheet1.cell(row=i, column=s, value=data_type_b).fill = filleN
+            if table_name_a == table_name_b and column_name_a == column_name_b and data_type_a != data_type_b:
+                sheet1.cell(row=r, column=c, value=data_type_b).fill = filleN  # 更新的单元格设置为绿色
                 break
-            if col1 == table_name_b and col2 == column_name_b and col3 == data_type_b:
-                sheet1.cell(row=i, column=s, value=data_type_b).fill = filleY
+            if table_name_a == table_name_b and column_name_a == column_name_b and data_type_a == data_type_b:
+                sheet1.cell(row=r, column=c, value=data_type_b).fill = filleY  # 无需更新的单元格设置为黄绿色
                 break
-            # ws.cell(row=i, column=7).value = 'varchar(2)'
-        # ws.cell(row=row, column=col).value =
-        i += 1
+
+        r += 1
 
 
 if __name__ == '__main__':
@@ -35,6 +36,7 @@ if __name__ == '__main__':
 
     # 复制excelA.xlsx为excelA_updated.xlsx
     shutil.copyfile(excelA_path, excelA_updated_path)
+
     workbook_b = load_workbook(excelB_path)
     worksheet_b = workbook_b.active
 
@@ -50,7 +52,4 @@ if __name__ == '__main__':
     deal(11)
     deal(15)
 
-        # 此处有一个注意事项
-        # cell(r,c)中的行r和列c 是从1开始。而不是0。  例如A11则是ws.cell(1,1),不是ws.cell(0,0)
-    # wb = openpyxl.load_workbook(xlsx_path)
     wb.save(excelA_updated_path)  # 设置保存的路径 和保存的文件名：2.xlsx
